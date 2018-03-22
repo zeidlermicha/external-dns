e7171aac88d47a8e6b879746bddc78f168b8bcbd
Michael.Zeidler <michael.zeidler@archibus-fm.de>
2018-03-22 19:01:13 +0100

    delete faulty imports

--- source/service.go
+++ source/service.go
@@ -28,7 +28,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/api/core/v1"
 
 	"github.com/kubernetes-incubator/external-dns/endpoint"
 )
